5264bf2ee9c5c4f49564349f45fac9607944b557
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2023-03-07 07:19:07 +0300

    .

--- Jenkins/app/script.sh
+++ Jenkins/app/script.sh
@@ -8,18 +8,18 @@ sudo apt-get install \
     gnupg \
     lsb-release -y
 
-sudo mkdir -m 0755 -p /etc/apt/keyrings
+sudo mkdir -m 0755 -p /etc/apt/keyrings -y || true
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg || true
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubu>  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubu>  $(lsb_release -cs) stable" |>
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y || true
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker samson
 
-sudo docker-compose up -d 
+sudo docker-compose up -d --build
 
-docker logs jenkins | less
+docker logs jenkins | less > /tmp/jenkins_init_passwd
 
-cat /var/jenkins_home/secrets/initialAdminPassword
+cat /tmp/jenkins_init_passwd
