a0b0e0842606217ccb19fcd6a646d3bacc780e0c
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2023-03-07 06:28:43 +0300

    .

--- Jenkins/app/script.sh
+++ Jenkins/app/script.sh
@@ -18,34 +18,8 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y || true
 sudo usermod -aG docker ubuntu
 
-docker network create jenkins || true
+sudo docker-compose up -d 
 
-docker run \
-  --name jenkins-docker \
-  --rm \
-  --detach \
-  --privileged \
-  --network jenkins \
-  --network-alias docker \
-  --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client \
-  --volume jenkins-data:/var/jenkins_home \
-  --publish 2376:2376 \
-  docker:dind \
-  --storage-driver overlay2
+docker logs jenkins | less
 
-docker build -t myjenkins-blueocean
-
-docker run \
-  --name jenkins-blueocean \
-  --restart=on-failure \
-  --detach \
-  --network jenkins \
-  --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client \
-  --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 \
-  --publish 50000:50000 \
-  --volume jenkins-data:/var/jenkins_home \
-  --volume jenkins-docker-certs:/certs/client:ro \
-  myjenkins-blueocean
+cat /var/jenkins_home/secrets/initialAdminPassword
