ec2155f5eeeb3004852baf4818c2a531b20d9030
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2023-03-07 10:49:21 +0300

    .

--- Jenkins/app/script.sh
+++ Jenkins/app/script.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-sudo apt-get update
-
-sudo apt-get install \
+sudo apt-get update || true
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release -y
+    lsb-release || true
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings || true
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg || true
-
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubu>  $(lsb_release -cs) stable" |>
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || true
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y || true
-sudo apt-get install docker-compose -y || true
-sudo usermod -aG docker samson
+sudo apt-get update || true
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin || true
+sudo apt-get install -y docker-compose || true
+sudo usermod -aG docker samson || true
 
 sudo docker-compose up -d --build
 
-docker logs jenkins | less > /tmp/jenkins_init_passwd
+sudo docker logs jenkins | less > /tmp/jenkins_init_passwd
 
-cat /tmp/jenkins_init_passwd
+sudo cat /tmp/jenkins_init_passwd
